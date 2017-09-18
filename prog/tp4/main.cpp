ab759594f887d71cf2ef610ce90af7607a470e11
Amaglio Adrian <adrian.amaglio@etu.univ-lyon1.fr>
2017-09-18 16:02:42 +0200

    :)

--- prog/tp4/main.cpp
+++ prog/tp4/main.cpp
@@ -36,36 +36,40 @@ T min(T const & a, T const & b){
 		return a;
 	return b;
 }
-template<>
-char *min(char *a, char *b){
+
+/* !!! char * différent de char[]b */
+template<const char*>
+char *min(const char *a, const char *b){
 	if(strcmp(a,b)<0)
 		return a;
 	return b;
 }
 
 bool test_min(){
-	if(min<double>(-4, 7) != -4)
-		return false;
-	if(min<std::string>("coucou", "whaou") != "coucou")
-		return false;
-	char s1[] = "coucou";
-	char s2[] = "wahou";
-	if(min<char*>(s1, s2) != s1)
-		return false;
-	if(min<char*>(s2, s1) != s1)
-		return false;
-
+	bool result = true;
+	char const *s1 = "coucou";
+	char const *s2 = "wahou";
 	char *hards1 = (char*)malloc(7);
 	strcpy(hards1, "coucou");
 	char *hards2 = (char*)malloc(7);
 	strcpy(hards2, "woucou");
+	
+	if(min<double>(-4, 7) != -4)
+		result = false;
+	else if(min<std::string>("coucou", "whaou") != "coucou")
+		result = false;
+	else if(strcmp(min<const char*>(s1, s2), s1) != 0)
+		result = false;
+	else if(strcmp(min<const char*>(s2, s1), s1) != 0)
+		result = false;
+	else if(min(hards1, hards2) != hards1)
+		result = false;
+	else if(min(hards2, hards1) != hards1)
+		result = false;
 
-	if(min(hards1, hards2) != hards1)
-		return false;
-	if(min(hards2, hards1) != hards1)
-		return false;
-
-	return true;
+	free(hards1);
+	free(hards2);
+	return result;
 }
 
 /***********************************************
@@ -112,11 +116,19 @@ public:
 	}
 
 	T & operator[](const int i){
+		if(i>=index)
+			std::cout << "Index out of bounds " << i << std::endl;
 		return elements[i];
 	}
 
 	void operator=(const Tableau t){
-
+		free(elements);
+		total_size = t.total_size;
+		index = t.index;
+		elements = (T*)malloc(t.total_size);
+		for (int i = 0; i < t.index; i++){
+			elements[i] = t.elements[i];
+		}
 	}
 
 friend bool test_tableau();
@@ -145,6 +157,9 @@ bool test_tableau(){
 		return false;
 	if(tint.total_size != 10)
 		return false;
+	tint2.push(3);
+	if(tint.index != 1)
+		return false;
 
 	if(tint[0] != 3)
 		return false;
@@ -166,6 +181,7 @@ bool test_tableau(){
 
 int main(int argc, char const *argv[])
 {
+	std::cout << "Test order matters" << std::endl;
 	std::cout << test_swap() << " : test_swap" << std::endl;
 	std::cout << test_min() << " : test_min" << std::endl;
 	std::cout << test_tableau() << " : test_tableau" << std::endl;
